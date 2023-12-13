fb1291c6273811a50340db06a852e03d982d2d12
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2023-12-13 13:51:05 +0100

    Make cookiecutter use the openbb-core

--- {{cookiecutter.project_tag}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_tag}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,20 +1,43 @@
 """{{ cookiecutter.package_name }} OpenBB Platform extension."""
+from pathlib import Path
+from typing import List, Optional, Union
 
-from openbb_core.provider.abstract.provider import Provider
-from {{cookiecutter.package_name}}.models.example import ExampleFetcher
+from openbb_core.app.static.app_factory import BaseApp as _BaseApp
+from openbb_core.app.static.app_factory import create_app as _create_app
+from openbb_core.app.static.package_builder import \
+    PackageBuilder as _PackageBuilder
 
-# mypy: disable-error-code="list-item"
+_this_dir = Path(__file__).parent.resolve()
+_PackageBuilder(_this_dir).auto_build()
 
-provider = Provider(
-    name="{{cookiecutter.package_name}}",
-    description="Data provider for {{cookiecutter.project_name}}.",
-    # Only add 'credentials' if they are needed.
-    # For multiple login details, list them all here.
-    # credentials=["api_key"],
-    website="https://{{cookiecutter.project_tag}}.com",
-    # Here, we list out the fetchers showing what our provider can get.
-    # The dictionary key is the fetcher's name, used in the `router.py`.
-    fetcher_dict={
-        "Example": ExampleFetcher,
-    }
-)
+
+def build(
+    modules: Optional[Union[str, List[str]]] = None,
+    lint: bool = True,
+    verbose: bool = False,
+) -> None:
+    """Build extension modules.
+
+    Parameters
+    ----------
+    modules : Optional[List[str]], optional
+        The modules to rebuild, by default None
+        For example: "/news" or ["/news", "/crypto"]
+        If None, all modules are rebuilt.
+    lint : bool, optional
+        Whether to lint the code, by default True
+    verbose : bool, optional
+        Enable/disable verbose mode
+    """
+    _PackageBuilder(_this_dir, lint, verbose).build(modules)
+
+
+try:
+    # pylint: disable=import-outside-toplevel
+    from {{ cookiecutter.package_name }}.package.__extensions__ import Extensions as _Extensions
+
+    obb: Union[_BaseApp, _Extensions] = _create_app(_Extensions)  # type: ignore
+    sdk = obb
+except (ImportError, ModuleNotFoundError):
+    print("Failed to import extensions.")
+    obb = sdk = _create_app()  # type: ignore
